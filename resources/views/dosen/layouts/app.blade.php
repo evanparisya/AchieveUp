4bc45f1378e97a9f21a1b6d92885169a1452771b
Kiaakk <krisna.wiliam26@gmail.com>
2025-06-01 02:31:45 +0700

    Update sidebar, app, and header for dosen section

--- resources/views/dosen/layouts/app.blade.php
+++ resources/views/dosen/layouts/app.blade.php
@@ -8,10 +8,17 @@
     <title>@yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css ">
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
+    {{-- Select2 CSS --}}
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+    {{-- Select2 JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
     @vite('resources/css/app.css')
 
 </head>
@@ -20,10 +27,10 @@
     <div class="flex min-h-screen">
         @include('dosen.layouts.sidebar')
 
-        <div class="flex-1 flex flex-col">
+        <div class="flex-1 flex flex-col ml-64"> 
             @include('dosen.layouts.header')
 
-            <main class="p-8">
+            <main class="p-8 mt-16 overflow-auto"> 
                 <div class="breadcrumb">
                     @include('dosen.layouts.breadcrumb')
                 </div>
@@ -33,8 +40,9 @@
             </main>
         </div>
     </div>
-    @vite('resources/js/app.js')
+    <script src="//unpkg.com/alpinejs" defer></script>
 
+    @vite('resources/js/app.js')
     <script>
         $.ajaxSetup({
             headers: {
@@ -43,7 +51,7 @@
         });
     </script>
 
-    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js "></script>
     @stack('js')
 </body>
 
