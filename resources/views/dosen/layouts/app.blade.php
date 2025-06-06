5a5946eb052c60914883c49286fca6b270b2c22c
Kiaakk <krisna.wiliam26@gmail.com>
2025-06-06 16:47:30 +0700

    Add notif dospem

--- resources/views/dosen/layouts/app.blade.php
+++ resources/views/dosen/layouts/app.blade.php
@@ -9,7 +9,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css ">
-
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     
